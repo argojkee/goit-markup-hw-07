ed633e6cc15088676642019d77718ccee60b3db5
argojkee <maksymhaidabura@gmail.com>
2023-07-17 15:06:30 +0300

    portfolio filter

---
+++ js/porfolio-items.js
@@ -0,0 +1,342 @@
+const cards = [
+  {
+    urlDesktopx1: './images/desktop-360-300-1px/desktop-1-1.jpg 1x',
+    urlDesktopx2: './images/desktop-360-300-2px/desktop-1-2.jpg 2x',
+    urlTabletx1: './images/tablet-356-300-1px/tablet-1-1.jpg 1x',
+    urlTabletx2: './images/tablet-356-300-2px/tablet-1-2.jpg 2x',
+    urlMobilex1: './images/mobile-396-280-1px/mobile-1-1.jpg 1x',
+    urlMobilex2: './images/mobile-396-280-2px/mobile-1-2.jpg 2x',
+    content_title: 'Banking App Interface Concept',
+    content_text: 'App',
+    alt: 'application',
+  },
+
+  {
+    urlDesktopx1: './images/desktop-360-300-1px/desktop-2-1.jpg 1x',
+    urlDesktopx2: './images/desktop-360-300-2px/desktop-2-2.jpg 2x',
+    urlTabletx1: './images/tablet-356-300-1px/tablet-2-1.jpg 1x',
+    urlTabletx2: './images/tablet-356-300-2px/tablet-2-2.jpg 2x',
+    urlMobilex1: './images/mobile-396-280-1px/mobile-2-1.jpg 1x',
+    urlMobilex2: './images/mobile-396-280-2px/mobile-2-2.jpg 2x',
+    content_title: 'Cashless Payment',
+    content_text: 'Marketing',
+    alt: 'telephone',
+  },
+
+  {
+    urlDesktopx1: './images/desktop-360-300-1px/desktop-3-1.jpg 1x',
+    urlDesktopx2: './images/desktop-360-300-2px/desktop-3-2.jpg 2x',
+    urlTabletx1: './images/tablet-356-300-1px/tablet-3-1.jpg 1x',
+    urlTabletx2: './images/tablet-356-300-2px/tablet-3-2.jpg 2x',
+    urlMobilex1: './images/mobile-396-280-1px/mobile-3-1.jpg 1x',
+    urlMobilex2: './images/mobile-396-280-2px/mobile-3-2.jpg 2x',
+    content_title: 'Meditation App',
+    content_text: 'App',
+    alt: 'monitor',
+  },
+  {
+    urlDesktopx1: './images/desktop-360-300-1px/desktop-4-1.jpg 1x',
+    urlDesktopx2: './images/desktop-360-300-2px/desktop-4-2.jpg 2x',
+    urlTabletx1: './images/tablet-356-300-1px/tablet-4-1.jpg 1x',
+    urlTabletx2: './images/tablet-356-300-2px/tablet-4-2.jpg 2x',
+    urlMobilex1: './images/mobile-396-280-1px/mobile-4-1.jpg 1x',
+    urlMobilex2: './images/mobile-396-280-2px/mobile-4-2.jpg 2x',
+    content_title: 'Taxi Service',
+    content_text: 'Marketing',
+    alt: 'car',
+  },
+  {
+    urlDesktopx1: './images/desktop-360-300-1px/desktop-5-1.jpg 1x',
+    urlDesktopx2: './images/desktop-360-300-2px/desktop-5-2.jpg 2x',
+    urlTabletx1: './images/tablet-356-300-1px/tablet-5-1.jpg 1x',
+    urlTabletx2: './images/tablet-356-300-2px/tablet-5-2.jpg 2x',
+    urlMobilex1: './images/mobile-396-280-1px/mobile-5-1.jpg 1x',
+    urlMobilex2: './images/mobile-396-280-2px/mobile-5-2.jpg 2x',
+    content_title: 'Screen Illustrationst',
+    content_text: 'Design',
+    alt: 'telephone screen',
+  },
+  {
+    urlDesktopx1: './images/desktop-360-300-1px/desktop-6-1.jpg 1x',
+    urlDesktopx2: './images/desktop-360-300-2px/desktop-6-2.jpg 2x',
+    urlTabletx1: './images/tablet-356-300-1px/tablet-6-1.jpg 1x',
+    urlTabletx2: './images/tablet-356-300-2px/tablet-6-2.jpg 2x',
+    urlMobilex1: '/images/mobile-396-280-1px/mobile-6-1.jpg 1x',
+    urlMobilex2: './images/mobile-396-280-2px/mobile-6-2.jpg 2x',
+    content_title: 'Online Courses',
+    content_text: 'Marketing',
+    alt: 'woomen',
+  },
+  {
+    urlDesktopx1: './images/desktop-360-300-1px/desktop-7-1.jpg 1x',
+    urlDesktopx2: './images/desktop-360-300-2px/desktop-7-2.jpg 2x',
+    urlTabletx1: './images/tablet-356-300-1px/tablet-7-1.jpg 1x',
+    urlTabletx2: './images/tablet-356-300-2px/tablet-7-2.jpg 2x',
+    urlMobilex1: './images/mobile-396-280-1px/mobile-7-1.jpg 1x',
+    urlMobilex2: './images/mobile-396-280-2px/mobile-7-2.jpg 2x',
+    content_title: 'Instagram Stories Concept',
+    content_text: 'Design',
+    alt: 'Instagramm content',
+  },
+  {
+    urlDesktopx1: './images/desktop-360-300-1px/desktop-8-1.jpg 1x',
+    urlDesktopx2: './images/desktop-360-300-2px/desktop-8-2.jpg 2x',
+    urlTabletx1: './images/tablet-356-300-1px/tablet-8-1.jpg 1x',
+    urlTabletx2: './images/tablet-356-300-2px/tablet-8-2.jpg 2x',
+    urlMobilex1: './images/mobile-396-280-1px/mobile-8-1.jpg 1x',
+    urlMobilex2: './images/mobile-396-280-2px/mobile-8-2.jpg 2x',
+    content_title: 'Organic Food',
+    content_text: 'Web Site',
+    alt: 'food',
+  },
+  {
+    urlDesktopx1: './images/desktop-360-300-1px/desktop-9-1.jpg 1x',
+    urlDesktopx2: './images/desktop-360-300-2px/desktop-9-2.jpg 2x',
+    urlTabletx1: './images/tablet-356-300-1px/tablet-9-1.jpg 1x',
+    urlTabletx2: './images/tablet-356-300-2px/tablet-9-2.jpg 2x',
+    urlMobilex1: './images/mobile-396-280-1px/mobile-9-1.jpg 1x',
+    urlMobilex2: './images/mobile-396-280-2px/mobile-9-2.jpg 2x',
+    content_title: 'Fresh Coffee',
+    content_text: 'Web Site',
+    alt: 'coffee',
+  },
+];
+
+const btnContainerEl = document.querySelector('.js-filter-list');
+const cardsListEl = document.querySelector('.js-content-list');
+const buttons = btnContainerEl.querySelectorAll('button');
+
+//Только одна категория
+// btnContainerEl.addEventListener('click', onButtonClick);
+
+// const markupString = makeMarkup(cards);
+// addElementsInUi(markupString);
+
+// function onButtonClick(e) {
+//   if (e.target.nodeName !== 'BUTTON') {
+//     return;
+//   }
+
+//   const selectedCategory = e.target.textContent;
+
+//   if (e.target.classList.contains('is-active')) {
+//     return;
+//   }
+
+//   if (selectedCategory === 'All') {
+//     const markupString = makeMarkup(cards);
+//     addElementsInUi(markupString);
+//   } else {
+//     const markupString = makeFilteredMarkup(cards, selectedCategory);
+//     addElementsInUi(markupString);
+//   }
+//   removeIsactive();
+//   addIsactive(e.target);
+// }
+
+// function makeMarkup(array) {
+//   let makeMarkupString = '';
+//   array.forEach(
+//     ({
+//       urlDesktopx1,
+//       urlDesktopx2,
+//       urlTabletx1,
+//       urlTabletx2,
+//       urlMobilex1,
+//       urlMobilex2,
+//       alt,
+//       content_title,
+//       content_text,
+//     }) => {
+//       makeMarkupString += ` <li class="content-list-item">
+//                 <a class="link photo-link" href=""><div class="parrent">
+//                     <picture>
+//                         <source srcset="${urlDesktopx1}, ${urlDesktopx2}"
+//                             media="(min-width: 1158px)">
+
+//                         <source srcset="${urlTabletx1}, ${urlTabletx2}"
+//                             media="(min-width: 768px)">
+
+//                         <source srcset="${urlMobilex1}, ${urlMobilex2}"
+//                             media="(min-width: 320px)">
+
+//                         <img src="./images/desktop-360-300-1px/desktop-9-1.jpg" alt="${alt}" width="360" height="300"
+//                             class="photo-content">
+//                     </picture>
+
+//                     <div class="overlay">
+//                         <p class="text-info">14 Stylish and User-Friendly App Design Concepts · Task Manager App · Calorie Tracker App ·
+//                             Exotic Fruit Ecommerce App ·
+//                             Cloud Storage App </p>
+//                     </div>
+//                     </div>
+//                 <div class="container-content-info">
+//                     <h2 class="content-list-title">${content_title}</h2>
+//                     <p class="content-list-text title-body">${content_text}</p>
+//                 </div></a>
+//             </li>`;
+//     }
+//   );
+//   return makeMarkupString;
+// }
+
+// function addElementsInUi(string) {
+//   cardsListEl.innerHTML = string;
+// }
+
+// function makeFilteredMarkup(array, category) {
+//   let makeMarkupString = '';
+//   const filteredMarkup = array.filter(
+//     element => element.content_text === category
+//   );
+//   return makeMarkup(filteredMarkup);
+// }
+
+// function removeIsactive() {
+//   buttons.forEach(button => {
+//     if (button.classList.contains('is-active')) {
+//       button.classList.remove('is-active');
+//     }
+//   });
+// }
+
+// function addIsactive(button) {
+//   button.classList.add('is-active');
+// }
+
+//Несколько категорий
+
+btnContainerEl.addEventListener('click', onButtonClick);
+
+addAllElementsInUi();
+
+function onButtonClick(e) {
+  if (e.target.nodeName !== 'BUTTON') {
+    return;
+  }
+
+  const selectedCategory = e.target.textContent;
+
+  if (selectedCategory === 'All') {
+    removeAllIsactive();
+
+    addIsactive(e.target);
+
+    addAllElementsInUi();
+  } else {
+    removeIsactive(buttons[0]);
+    e.target.classList.toggle('is-active');
+
+    let isAnyActive = false;
+    let countSelected = 0;
+
+    for (let i = 0; i < buttons.length; i += 1) {
+      if (buttons[i].classList.contains('is-active')) {
+        isAnyActive = true;
+        countSelected += 1;
+      }
+    }
+
+    if (!isAnyActive || countSelected === buttons.length - 1) {
+      if (countSelected === buttons.length - 1) {
+        removeAllIsactive();
+      }
+      addIsactive(buttons[0]);
+
+      addAllElementsInUi();
+    } else {
+      const activeButtons = [];
+      for (const button of buttons) {
+        if (button.classList.contains('is-active')) {
+          activeButtons.push(button);
+        }
+      }
+
+      let markupString = '';
+      for (const button of activeButtons) {
+        markupString += makeFilteredMarkup(cards, button.textContent);
+      }
+
+      addElementsInUi(markupString);
+    }
+  }
+}
+
+function makeMarkup(array) {
+  let makeMarkupString = '';
+  array.forEach(
+    ({
+      urlDesktopx1,
+      urlDesktopx2,
+      urlTabletx1,
+      urlTabletx2,
+      urlMobilex1,
+      urlMobilex2,
+      alt,
+      content_title,
+      content_text,
+    }) => {
+      makeMarkupString += ` <li class="content-list-item">
+                <a class="link photo-link" href=""><div class="parrent">
+                    <picture>
+                        <source srcset="${urlDesktopx1}, ${urlDesktopx2}"
+                            media="(min-width: 1158px)">
+
+                        <source srcset="${urlTabletx1}, ${urlTabletx2}"
+                            media="(min-width: 768px)">
+
+                        <source srcset="${urlMobilex1}, ${urlMobilex2}"
+                            media="(min-width: 320px)">
+
+                        <img src="./images/desktop-360-300-1px/desktop-9-1.jpg" alt="${alt}" width="360" height="300"
+                            class="photo-content">
+                    </picture>
+
+                    <div class="overlay">
+                        <p class="text-info">14 Stylish and User-Friendly App Design Concepts · Task Manager App · Calorie Tracker App ·
+                            Exotic Fruit Ecommerce App ·
+                            Cloud Storage App </p>
+                    </div>
+                    </div>
+                <div class="container-content-info">
+                    <h2 class="content-list-title">${content_title}</h2>
+                    <p class="content-list-text title-body">${content_text}</p>
+                </div></a>
+            </li>`;
+    }
+  );
+  return makeMarkupString;
+}
+
+function addElementsInUi(string) {
+  cardsListEl.innerHTML = string;
+}
+
+function makeFilteredMarkup(array, category) {
+  const filteredMarkup = array.filter(
+    element => element.content_text === category
+  );
+  return makeMarkup(filteredMarkup);
+}
+
+function removeAllIsactive() {
+  buttons.forEach(button => {
+    if (button.classList.contains('is-active')) {
+      button.classList.remove('is-active');
+    }
+  });
+}
+
+function addIsactive(button) {
+  button.classList.add('is-active');
+}
+
+function removeIsactive(button) {
+  button.classList.remove('is-active');
+}
+
+function addAllElementsInUi() {
+  const markupString = makeMarkup(cards);
+
+  addElementsInUi(markupString);
+}
