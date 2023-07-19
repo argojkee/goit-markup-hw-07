4227c67894947fe0d5ca33d0896469955aeb6f34
argojkee <maksymhaidabura@gmail.com>
2023-07-19 13:44:48 +0300

    update backdrop

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,47 @@
+const refs = {
+  openModalBtn: document.querySelector('[data-modal-open]'),
+  closeModalBtn: document.querySelector('[data-modal-close]'),
+  modal: document.querySelector('[data-modal]'),
+  bodyEl: document.querySelector('body'),
+};
+
 (() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-  };
-
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
+  refs.openModalBtn.addEventListener('click', () => {
+    refs.bodyEl.addEventListener('keydown', onEscPress);
+    refs.bodyEl.addEventListener('click', onBackdropClick);
+    toggleModal();
+  });
+  refs.closeModalBtn.addEventListener('click', () => {
+    backdropRemoveEventListener();
+    toggleModal();
+  });
 })();
+
+function toggleModal() {
+  refs.modal.classList.toggle('is-hidden');
+}
+
+// Backdrop
+
+function onEscPress(event) {
+  if (event.code !== 'Escape') {
+    return;
+  }
+
+  refs.modal.classList.add('is-hidden');
+  backdropRemoveEventListener();
+}
+
+function onBackdropClick() {
+  if (event.target !== refs.modal) {
+    return;
+  }
+
+  backdropRemoveEventListener();
+  toggleModal();
+}
+
+function backdropRemoveEventListener() {
+  refs.bodyEl.removeEventListener('keydown', onEscPress);
+  refs.bodyEl.removeEventListener('click', onBackdropClick);
+}
